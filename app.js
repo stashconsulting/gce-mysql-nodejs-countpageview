3128a9051e2a648a5c19219a398ccf0e14ade223
syler023 <gen23.al@gmail.com>
2020-01-08 22:27:55 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+const util = require('util');
+
+var app = express();
+
+var connection = mysql.createConnection({
+   host     : process.env.host,
+   user     : process.env.user,
+   password : process.env.password,
+   database : process.env.database
+ });
+
+main = async () => {
+   let query = "SELECT count_view FROM view"
+   let query_engine =  util.promisify(connection.query).bind(connection);
+   let result = await query_engine(query)
+   if (result[0] == "NULL"){
+     query = "INSERT INTO view (page_view) VALUES (1)"
+     result = 1
+   } else {
+     result = result[0]['count_view'] + 1
+     query = `UPDATE view SET count_view = ${result};`
+   }
+   query_engine(query)
+   
+   return result
+  
+ }
+
+app.get('/', function (req, res) {
+  main().then(data => {
+    res.send('count is'+ `${data}`);
+   });
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port 
+  console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ startup_script.bash
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Install dependencies from apt
+sudo apt update -y
+sudo apt install git -y
+
+# Install nodejs
+mkdir /opt/nodejs
+curl https://nodejs.org/dist/v8.12.0/node-v8.12.0-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
+ln -s /opt/nodejs/bin/node /usr/bin/node
+ln -s /opt/nodejs/bin/npm /usr/bin/npm
+
+#Install SQL Proxy
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+
+#Initialize the Cloud SQL instance
+./cloud_sql_proxy -instances="stone-door-258502:us-west1:instance2"=tcp:3306
+
+# Get the application source code
+git clone https://github.com/stashconsulting/nodejsdemo.git
+cd ./nodejsdemo/
+
+# Install app dependencies
+npm install
+
+#run app
+node app.js &
